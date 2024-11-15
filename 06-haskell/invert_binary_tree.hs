629dd20b881d09809dc2b9b80b79615ccece809c
Max Goryunov <maximthehedgehog@yandex.ru>
2024-11-16 00:06:33 +0300

    add program

---
+++ 06-haskell/invert_binary_tree.hs
@@ -0,0 +1,27 @@
+data Tree a = Empty
+            | Node a (Tree a) (Tree a)
+            deriving (Show)
+
+
+invertTree :: Tree a -> Tree a
+invertTree Empty = Empty
+invertTree (Node value left right) =
+    Node value (invertTree right) (invertTree left)
+
+
+printTree :: Show a => Tree a -> IO ()
+printTree tree = printTree' tree 0
+  where
+    printTree' Empty _ = return ()
+    printTree' (Node value left right) level = do
+        printTree' right (level + 1)
+        putStrLn $ replicate (5 * level) ' ' ++ show value 
+        printTree' left (level + 1)
+
+main :: IO ()
+main = do
+    let tree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 Empty Empty)
+    putStrLn "Original Tree:"
+    print $ invertTree tree 
+    putStrLn "Tree Structure:"
+    printTree tree
