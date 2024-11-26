d5736ea61f3192a0853f8a6b3c1b137c0593e6d5
Max Goryunov <maximthehedgehog@yandex.ru>
2024-11-26 20:31:44 +0300

    cleaning

--- 05-ocaml/golden_search.ml
+++ 05-ocaml/golden_search.ml
@@ -1,15 +1,15 @@
 let golden_section_search f a b tol =
   let phi = (1.0 +. sqrt 5.0) /. 2.0 in
   let resphi = 1.0 /. phi in
-  Printf.printf " %.6f \n" resphi;
+  Printf.printf "resphi = %.6f \n" resphi;
   let neg_inf = neg_infinity in
   (* let resphi = 2.0 -. phi in *)
 
   let rec search a b x_1 x_2 =
-    Printf.printf "a = %.6f \n" a;
-    Printf.printf "b = %.6f \n" b;
-    Printf.printf "x_1 = %.6f \n" x_1;
-    Printf.printf "x_2 = %.6f \n" x_2;
+    Printf.printf "a = %.6f " a;
+    Printf.printf "b = %.6f " b;
+    Printf.printf "x_1 = %.6f " x_1;
+    Printf.printf "x_2 = %.6f\n" x_2;
     if abs_float (b -. a) < tol then
       (a +. b) /. 2.0 
     else
@@ -35,6 +35,6 @@ let example_function x =
 let () =
   let a = 0.0 in
   let b = 4.0 in
-  let tolerance = 1e-5 in
+  let tolerance = 1e-1 in
   let minimum = golden_section_search example_function a b tolerance in
   Printf.printf "The minimum is at x = %f with value f(x) = %f\n" minimum (example_function minimum)
