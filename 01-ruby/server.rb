4e137cef20ac288c329ff592e9074b1ccaa998ab
Max Goryunov <maximthehedgehog@yandex.ru>
2024-12-11 02:03:45 +0300

    client id

--- 01-ruby/server.rb
+++ 01-ruby/server.rb
@@ -2,16 +2,22 @@ require 'socket'
 require 'thread'
 
 server = TCPServer.new('localhost', 1234)
-puts "Chat server is running on localhost:1234"
+puts "Address: localhost:1234"
 
 clients = []
+client_ids = {}
 mutex = Mutex.new
+client_counter = 0
 
 Thread.new do
   loop do
     Thread.start(server.accept) do |client|
-      mutex.synchronize { clients << client }
-      puts "Client connected: #{client.peeraddr.inspect}"
+      mutex.synchronize do
+        client_counter += 1
+        clients << client
+        client_ids[client] = client_counter
+      end
+      puts "Client connected: #{client.peeraddr.inspect} with ID: #{client_counter}"
 
       begin
         loop do
@@ -19,7 +25,8 @@ Thread.new do
           break if message.nil?
           message.chomp!
           timestamp = Time.now.strftime("%H:%M:%S")
-          formatted_message = "#{timestamp} - #{message}"
+          client_id = client_ids[client]
+          formatted_message = "#{timestamp} - [Client #{client_id}] #{message}"
           puts "Received message: #{formatted_message}"
           mutex.synchronize {
             clients.each do |c|
@@ -32,6 +39,7 @@ Thread.new do
       ensure
         mutex.synchronize {
           clients.delete(client)
+          client_ids.delete(client)
         }
         puts "Client disconnected: #{client.peeraddr.inspect}"
         client.close
